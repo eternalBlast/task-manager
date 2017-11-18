44a5319056d58ef417d80043467aff0ca2b677ca
Andri <wuandri@rocketmail.com>
2017-11-18 22:03:24 +0700

    Initial commit

---
+++ app/src/main/java/com/professional/andri/taskmanager/BaseActivity.java
@@ -0,0 +1,17 @@
+package com.professional.andri.taskmanager;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Andri on 18/11/2017.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
---
+++ app/src/main/java/com/professional/andri/taskmanager/TaskListActivity.java
@@ -0,0 +1,8 @@
+package com.professional.andri.taskmanager;
+
+/**
+ * Created by Andri on 18/11/2017.
+ */
+
+public class TaskListActivity extends BaseActivity {
+}
